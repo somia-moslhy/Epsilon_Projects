e5c1ec47ebef2d77a4ee358c77eac4c63fc63e8d
Somia <somiamoslhy@gmail>
2026-02-05 00:07:19 +0200

    Feat: Add game items configration dictionary

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+# Configuration: Items and their properties
+items = {
+    "medicine": {"disaster": "the plague", "base damage": 40, "Durability": 1},
+    "Canned food": {"disaster": "Great Famine", "base damage": 30, "Durability": 1},
+    "Reinforced Wood": {"disaster": "Hurricane", "base damage": 25, "Durability": 2},
+    "Steel Shield": {"disaster": "Bandit Ambush", "base damage": 20, "Durability": 3}
+}
+
+if __name__ == "__main__":
+    print("Game is ready to start ...")
