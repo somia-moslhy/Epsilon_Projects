108e967c33990a14904cc0dabbe0d8243db69f40
Somia <somiamoslhy@gmail>
2026-02-05 00:35:02 +0200

    Feat: Implement core disaster logic and durability check

--- functions.py
+++ functions.py
@@ -30,5 +30,27 @@ def take_bids(players):
                 except ValueError:
                     print("Please enter a valid number.")
 
+def system_game(players, disaster, damage,day,item_needed):
+    """
+    Checks if players have the required item to survive the disaster .
+    Updates health and item durability.
+    """
+    for name,data in players.items():
+        if data["health"]>0:
+            continue
 
+        # Logic: Check directly in the inv dict
+        if item_needed in data['inv'] and data['inv'][item_needed]>0:
+            print(f"Player {name} survived using {item_needed}!")
 
+            # Reduce durability
+            data['inv'][item_needed]-=1
+
+            # remove item if broken
+            if data['inv'][item_needed]==0:
+                del data['inv'][item_needed]
+        else:
+            # Apply damage
+            total_damage=damage + (day*5)
+            data['health']-=total_damage
+            print(f"{name} took {total_damage} damage from {disaster}. Health is now {data['health']}.")
